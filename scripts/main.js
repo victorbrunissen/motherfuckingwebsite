2e747d8d6524776aadc9aeadb0a28b2cd31ea365
victorbrunissen <victorbrunissen@gmail.com>
2019-01-11 09:41:21 +0100

    Modification script main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
+/*setTimeout(window.confirm(),5000);
+
+if (window.confirm('If you click "ok" you would be redirected . Cancel will load this website ')) 
+{
+window.location.href='https://www.google.com/chrome/browser/index.html';
+};
+*/
+/*
 setTimeout(function() {
-	alert ('Hey, le site xxxvidsxxx est trop bien. Viens dessus stp please')
-}, 10000);
+	window.confirm ('Hey, le site xxxvidsxxx est trop bien. Viens dessus stp please')
+}, 10000);*/
+
+setTimeout(function(){
+	window.confirm ('Hey, le site xxxvidsxxx est trop bien. Clique sur ok pour y aller stp please')
+
+	window.location.href='https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+},5000);
